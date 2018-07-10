0373f62516579ae2433595d5b5890057e6523b21
GeorgiSap <georgisapundzhiev@hotmail.com>
2018-07-10 14:41:03 +0300

    Improved code style

--- sw.js
+++ sw.js
@@ -1,40 +1,39 @@
 let staticCacheName = 'rest-reviews-static-v1';
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			if (response) return response;
-			return fetch(event.request);
-		})
-		)
-});
+self.addEventListener('fetch', (event) =>
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) return response;
+            return fetch(event.request);
+        })
+    )
+);
 
-self.addEventListener('activate', function(event) {
-	event.waitUntil(
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.filter(cacheName => 
-					cacheName.startsWith('rest-reviews') && cacheName != staticCacheName)
-				.map(cacheName => caches.delete(cacheName))
-				)
-		})
-		)
-});
+self.addEventListener('activate', (event) =>
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter(cacheName =>
+                    cacheName.startsWith('rest-reviews') && cacheName != staticCacheName)
+                .map(cacheName => caches.delete(cacheName))
+            )
+        })
+    )
+);
 
-self.addEventListener('install', function(event) {
-	console.log('Install event triggered')
-	event.waitUntil(
-		caches.open(staticCacheName).then(function(cache) {
-			return cache.addAll([
-				'/',
-				'/restaurant.html',
-				'/css/styles.css',
-				'/js/main.js',
-				'/js/restaurant_info.js',
-				'/js/dbhelper.js',
-				'/fonts/lobster.otf',
-				'/data/restaurants.json'
-				])
-		})
-		);
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/restaurant.html',
+                '/css/styles.css',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/js/dbhelper.js',
+                '/fonts/lobster.otf',
+                '/data/restaurants.json'
+            ])
+        })
+    );
 });
